a3bb5393903c7b6ce98da741fa17105a5a7f6bb2
cophilot <philxsb@gmail.com>
2024-03-12 10:34:40 +0100

    update: Tue 12 Mar 2024 10:34:40 CET

---
+++ compile.py
@@ -0,0 +1,7 @@
+"""
+compile all src files into a single script by getting the text from the files in src end 
+extrating between '###START###' and '###END###' and 
+writing them into the msh file ate the '###FUNCTIONS###' tag
+
+Copy the content from the .phil-project into msh to store environment variables
+"""
